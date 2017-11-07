49e532918d1c9002036bd75572c19f0e1d965f8a
Scott Fitzgerald <scott@droolcup.com>
2017-11-07 12:25:08 -0500

    Added bootstrap examples

--- week9.1/js/07a_example.js
+++ week9.1/js/07a_example.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // find all elements that are headers (<h1> ... <h6>) and apply the headline class to them
     $(":header").addClass("headline");
-    // take the first three elements in a list. hide them. then fadein over three seconds
+    // take every element in this list. hide them. then fadein over three seconds
     $("li").hide().fadeIn(3000);
     // when a list item is clicked on, remove it fromt he DOM
     $("li").on("click", function () {
